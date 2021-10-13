25b328577477095991cd397f09d1c72e855d5711
Crowe7 <jacobocrowe@gmail.com>
2021-10-13 05:11:02 -0500

    added basic dark mode button and fixed body css

---
+++ script.js
@@ -0,0 +1,11 @@
+function darkMode() {
+    let drk = document.body
+    if(drk.classList.contains("dark")) {
+        drk.classList.remove("dark");
+    }
+    else{
+    drk.classList.add("dark");
+    }
+}
+
+
