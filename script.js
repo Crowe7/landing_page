db7a8c8b9d514501b23d34717716e06d6ae6e6c2
Crowe7 <jacobocrowe@gmail.com>
2021-10-19 04:58:38 -0500

    Changed dark mode js again

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ function darkMode() {
     let drk = document.body
     if(drk.classList.contains("dark")) {
         drk.classList.remove("dark");
+        document.querySelector("#bttn").innerHTML = "Dark Mode";
     }
     else{
     drk.classList.add("dark");
+    document.querySelector("#bttn").innerHTML = "Light Mode";
     }
 }
 
