cd2fbbb35df50f0ae5a2d367434622cfdf90e8db
Crowe7 <jacobocrowe@gmail.com>
2021-10-24 05:38:19 -0500

    added dark mode memory
    saves the website on dark mode if thats what you last had selected

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 function darkMode() {
-    let drk = document.body
+    let drk = document.body;
     if(drk.classList.contains("dark")) {
         drk.classList.remove("dark");
         document.querySelector("#bttn").innerHTML = "Dark Mode";
+        setDark();
     }
     else{
     drk.classList.add("dark");
     document.querySelector("#bttn").innerHTML = "Light Mode";
+    setDark();
     }
 }
-
-
+function setDark(){
+    let set = document.getElementsByClassName("dark");
+    if( set.length > 0){
+        localStorage.setItem("mode", "dark");       
+    }
+    else{
+        localStorage.setItem("mode", "light");
+    }
+}
+let sve = localStorage.getItem("mode");
+function load(){
+    if(sve === "dark") {
+       darkMode();
+    }
+}
+window.onload = load();
